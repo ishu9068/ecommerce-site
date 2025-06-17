f9b3774e57bc6a382607e8c35d1a6b748f0cf23c
ishu9068 <ishupal885@gmail.com>
2025-06-17 14:34:03 +0530

    Initial commit with project structure and code

---
+++ server/app.js
@@ -0,0 +1,28 @@
+﻿const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, '..', 'src')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'src', 'pages', 'index.html'));
+});
+
+app.get('/products', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'src', 'pages', 'products.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'src', 'pages', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'src', 'pages', 'contact.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}
+);
---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+  fetch('/data/products.json')
+    .then(res => res.json())
+    .then(products => {
+      const container = document.getElementById('product-list');
+      if (!container) return;
+      products.forEach(p => {
+        const div = document.createElement('div');
+        div.className = 'product';
+        div.innerHTML = 
+          <img src='' alt='' width='200'><br>
+          <h2></h2>
+          <p></p>
+          <p></p>
+        ;
+        container.appendChild(div);
+      });
+    });
+});
