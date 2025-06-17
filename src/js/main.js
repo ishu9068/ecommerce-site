fc43bc6adab48167794a8d3393ce1dc300dd1ba1
ishu9068 <ishupal885@gmail.com>
2025-06-17 15:43:03 +0530

    Added ishuu

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,18 @@
     .then(products => {
       const container = document.getElementById('product-list');
       if (!container) return;
+
       products.forEach(p => {
         const div = document.createElement('div');
         div.className = 'product';
-        div.innerHTML = 
-          <img src='' alt='' width='200'><br>
-          <h2></h2>
-          <p></p>
-          <p></p>
-        ;
+        div.innerHTML = `
+          <img src="${p.image}" alt="${p.title}" width="200"><br>
+          <h2>${p.title}</h2>
+          <p>${p.description}</p>
+          <p>Price: $${p.price}</p>
+        `;
         container.appendChild(div);
       });
-    });
+    })
+    .catch(err => console.error('Error fetching products:', err));
 });
